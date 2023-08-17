409366796edae44994debfcadd18c4b9a1b64f0b
Mike Liu <mliu@atlassian.com>
2023-08-17 14:50:43 +1000

    Add completed code for 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,11 +6,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
   )
 }
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 export default App
