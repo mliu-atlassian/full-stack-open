99e2148fff1dbcaf6c0d59beca86c5e62ddb8eee
Mike Liu <mliu@atlassian.com>
2023-08-30 20:31:46 +1000

    Add completed code for 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,18 +6,33 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handlePersonChange} value={newName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
+      <div>debug: {newName}</div>
       <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
