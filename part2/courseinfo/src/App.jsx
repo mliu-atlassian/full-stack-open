1a087812acbc5b7db3a81a6b3a25bd02d5cb5752
Mike Liu <mliu@atlassian.com>
2023-08-30 15:12:01 +1000

    Add completed code for 2.2 & 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -10,10 +10,18 @@ const Content = ({ parts }) =>
     {parts.map(part => <Part key={part.id} part={part} />)}
   </>
 
+const Total = ({ parts }) => {
+  const total = parts.map(part => part.exercises).reduce((sum, val) => sum + val)
+  return <b>
+    total of {total} exercises
+  </b>
+}
+
 const Course = ({ course }) =>
   <>
     <Header text={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </>
 
 const App = () => {
