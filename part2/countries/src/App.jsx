0ec11148a6f7816a367f4e4de9ff32cf606c53d0
Mike Liu <mliu@atlassian.com>
2023-09-15 15:48:02 +1000

    Add completed code for 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
 import countryService from './services/countries'
 
-const Countries = ({ search, countries }) => {
+const Countries = ({ search, countries, country, setCountry }) => {
+  if (country) return <Country country={countries.find(c => c.name.common === country)} />
   if (search) {
     const results = countries.map(country => country.name.common).sort().filter(name => name.toLowerCase().includes(search.toLowerCase()))
     if (results.length > 10) return <div>Too many matches, specify another filter</div>
     if (results.length === 1) return <Country country={countries.find(country => country.name.common === results[0])} />
-    return results.map(name => <div key={name}>{name}</div>)
+    return results.map(name => <div key={name}>{name}<button onClick={() => setCountry(name)}>show</button></div>)
   }
 }
 
@@ -23,16 +24,20 @@ const Country = ({ country }) => {
 
 function App() {
   const [search, setSearch] = useState('')
+  const [country, setCountry] = useState('')
   const [countries, setCountries] = useState([])
 
   useEffect(() => { countryService.getAll().then(initialCountries => setCountries(initialCountries)) }, [])
 
-  const handleSearchChange = event => setSearch(event.target.value)
+  const handleSearchChange = event => {
+    setSearch(event.target.value)
+    setCountry('')
+  }
 
   return (
     <>
       <div>find countries <input onChange={handleSearchChange} value={search} /></div>
-      <Countries search={search} countries={countries} />
+      <Countries search={search} countries={countries} country={country} setCountry={setCountry} />
     </>
   )
 }
