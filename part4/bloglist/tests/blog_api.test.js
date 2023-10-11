abcdb5db936fbc5466ae1661953db2a3cd674647
Mike Liu <mliu@atlassian.com>
2023-10-11 23:59:11 +1100

    Add completed code for 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('when there is initially some notes saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(b => b.title)
+    expect(titles).toContain('TDD harms architecture')
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
