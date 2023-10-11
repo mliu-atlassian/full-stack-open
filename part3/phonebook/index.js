5444e47d34125ec4dcf356a0447ce89b6855fba9
Mike Liu <mliu@atlassian.com>
2023-10-11 15:28:04 +1100

    Add completed code for 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -64,9 +64,8 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
 })
 
 const PORT = process.env.PORT
